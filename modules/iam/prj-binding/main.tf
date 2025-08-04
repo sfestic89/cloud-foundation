9bef6ca6042a94d48d02e3e71e0911fd5f5b690c
sfestic <sfestic@gomakeit.net>
2025-08-04 10:54:52 -0400

    Refactor LZ Code

--- modules/iam/prj-binding/main.tf
+++ modules/iam/prj-binding/main.tf
@@ -1,7 +1,29 @@
+/**
 resource "google_project_iam_member" "project_roles" {
   for_each = toset(var.roles)
 
   project = var.project_id
   role    = each.value
   member  = var.member
+}
+**/
+locals {
+  bindings = flatten([
+    for role, members in var.iam_bindings : [
+      for member in members : {
+        role   = role
+        member = member
+      }
+    ]
+  ])
+}
+
+resource "google_project_iam_member" "project_roles" {
+  for_each = {
+    for b in local.bindings : "${b.role}-${b.member}" => b
+  }
+
+  project = var.project_id
+  role    = each.value.role
+  member  = each.value.member
 }
