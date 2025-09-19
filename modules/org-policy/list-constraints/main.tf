d79dd0d17f957ef0134017a7c6d7654027a65431
sfestic89 <sfestic@hotmail.com>
2025-09-19 08:37:55 -0400

    New list-constraints ORG Policy.

--- modules/org-policy/list-constraints/main.tf
+++ modules/org-policy/list-constraints/main.tf
@@ -40,8 +40,8 @@ resource "google_org_policy_policy" "list_constraint_policy" {
       content {
         values {
           # Choose the fallback behavior for non-matching resources.
-          allowed_values = try(each.value.else_behavior, "allow_all") == "allowed_values" ? true : null
-          denied_values  = try(each.value.else_behavior, "allow_all") == "denied_values" ? true : null
+          allowed_values = try(each.value.else_behavior, "allow_all") == "allow_all" ? true : null
+          denied_values  = try(each.value.else_behavior, "allow_all") == "deny_all" ? true : null
           
         }
       }
